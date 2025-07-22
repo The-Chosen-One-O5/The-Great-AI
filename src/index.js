7a937bb167e4964ac5854494bad03fa5f6a3ab6f
The-Chosen-One-O5 <shakthinathan2008@gmail.com>
2025-07-22 13:12:34 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // <-- THIS LINE SHOULD BE HERE
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // <-- ADD THIS LINE
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,7 +15,7 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register(); // <-- AND THIS LINE SHOULD BE HERE
+serviceWorkerRegistration.register(); // <-- ADD THIS LINE
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
